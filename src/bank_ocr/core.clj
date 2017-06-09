902bad22c97a0fffe0b656368b08d1b3448b35f7
defndaines <defndaines@gmail.com>
2017-06-08 18:40:35 -0700

    Adds use case 1 tests

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -1,6 +1,6 @@
 (ns bank-ocr.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse
+  "Convert an entry into an account number."
+  [entry]
+  111111111)
