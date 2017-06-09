4fa14d3093bb80ebbab13b1f54215c48047f1ea0
defndaines <defndaines@gmail.com>
2017-06-08 18:21:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Bank OCR
+
+A Clojure implementation of the [Bank OCR](http://codingdojo.org/kata/BankOCR/)
+kata.
+
+
+## Usage
+
+FIXME
+
+
+## License
+
+Copyright © 2017 Michael S. Daines
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject bank-ocr "0.1.0-SNAPSHOT"
+  :description "Clojure implementation of Bank OCR kata."
+  :url "https://github.com/defndaines/bank-ocr"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
