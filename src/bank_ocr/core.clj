5f2edb5efe98f4bbd9a73f7c0c1fd0399848249f
defndaines <defndaines@gmail.com>
2017-06-09 19:25:56 -0700

    Parses 3-line entries to account numbers

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -1,6 +1,40 @@
-(ns bank-ocr.core)
+(ns bank-ocr.core
+  "Functions to solve the Bank OCR kata.")
+
+
+;; Entry: A sequence of three lines which represent an account number.
+;; Account Number: A sequence of 9 digits identifying an account.
+
+
+(def digit-map
+  "Mapping of the 9-character sequences that makes up each digit."
+  {[\space \_ \space \| \space \| \| \_ \|] 0
+   [\space \space \space \space \space \| \space \space \|] 1
+   [\space \_ \space \space \_ \| \| \_ \space] 2
+   [\space \_ \space \space \_ \| \space \_ \|] 3
+   [\space \space \space \| \_ \| \space \space \|] 4
+   [\space \_ \space \| \_ \space \space \_ \|] 5
+   [\space \_ \space \| \_ \space \| \_ \|] 6
+   [\space \_ \space \space \space \| \space \space \|] 7
+   [\space \_ \space \| \_ \| \| \_ \|] 8
+   [\space \_ \space \| \_ \| \space \_ \|] 9})
+
+
+(defn- pad
+  "Ensure that a line has 27 characters by padding with spaces.
+  This normalizes the top line, which can empty above '1' and '4'."
+  [line]
+  (take 27 (concat line (repeat \space))))
+
 
 (defn parse
-  "Convert an entry into an account number."
+  "Converts an entry into an account number."
   [entry]
-  111111111)
+  (->> entry
+       (map pad)
+       (map #(partition-all 3 %))
+       (apply map vector)
+       (map flatten)
+       (map #(get digit-map %))
+       ;; TODO Remove the String representation from this function.
+       (apply str)))
